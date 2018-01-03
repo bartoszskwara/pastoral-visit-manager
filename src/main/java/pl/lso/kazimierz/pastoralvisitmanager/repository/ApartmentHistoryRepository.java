ce4cc7988867ff4f449b8bea75b146f6f1dd3ae2
bartoszskwara <bartoszskwara95@gmail.com>
2018-01-03 23:04:14 +0100

    Fix typo in ApartmentRepository

--- src/main/java/pl/lso/kazimierz/pastoralvisitmanager/repository/ApartmentHistoryRepository.java
+++ src/main/java/pl/lso/kazimierz/pastoralvisitmanager/repository/ApartmentHistoryRepository.java
@@ -1,6 +1,7 @@
 package pl.lso.kazimierz.pastoralvisitmanager.repository;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import pl.lso.kazimierz.pastoralvisitmanager.model.entity.ApartmentHistory;
 
-public interface ApartmentHistoryRepository extends PagingAndSortingRepository<ApartmentRepository, Long> {
+public interface ApartmentHistoryRepository extends PagingAndSortingRepository<ApartmentHistory, Long> {
 }

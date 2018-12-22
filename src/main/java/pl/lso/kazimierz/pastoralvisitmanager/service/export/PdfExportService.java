4755e4221f85df8df09e3eb66ed250921329ac25
Bartosz Skwara <bartoszskwara95@gmail.com>
2018-12-22 21:29:32 +0100

    Fix sorting in export PDF

--- src/main/java/pl/lso/kazimierz/pastoralvisitmanager/service/export/PdfExportService.java
+++ src/main/java/pl/lso/kazimierz/pastoralvisitmanager/service/export/PdfExportService.java
@@ -9,6 +9,7 @@ import com.itextpdf.text.Phrase;
 import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 import pl.lso.kazimierz.pastoralvisitmanager.model.dto.address.SelectedAddress;
 import pl.lso.kazimierz.pastoralvisitmanager.model.dto.pastoralvisit.PastoralVisitStatus;
@@ -94,7 +95,21 @@ public class PdfExportService extends ZipExportService {
 
     private void createRows(PdfPTable table, SelectedAddress selectedAddress) {
         int count = 1;
-        for(Apartment apartment : selectedAddress.getAddress().getApartments()) {
+
+        List<Apartment> apartments = selectedAddress.getAddress().getApartments();
+        apartments.sort((o1, o2) -> {
+            Integer num1;
+            Integer num2;
+            try {
+                num1 = Integer.parseInt(o1.getNumber().replaceAll("[\\D]", ""));
+                num2 = Integer.parseInt(o2.getNumber().replaceAll("[\\D]", ""));
+            } catch (Exception e) {
+                return StringUtils.compare(o1.getNumber(), o2.getNumber());
+            }
+            return num1.compareTo(num2);
+        });
+
+        for(Apartment apartment : apartments) {
             table.addCell(cell(String.valueOf(count)));
             table.addCell(cell(apartment.getNumber()));
 
